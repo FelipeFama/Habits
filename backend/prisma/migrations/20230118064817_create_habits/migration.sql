87344a1c0f657892561dd170b920e6eebed442dc
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-01-19 13:42:44 -0300

    Create backend

---
+++ backend/prisma/migrations/20230118064817_create_habits/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "habits" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "tittle" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL
+);
---
+++ backend/src/server.ts
@@ -0,0 +1,24 @@
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
+
+const app = Fastify();
+const prisma = new PrismaClient();
+
+/* HTTP methods: GET, POST, PUT, Patch and Delete */
+
+app.register(cors);
+
+app.get("/hello", async () => {
+  const habits = await prisma.habit.findMany();
+
+  return habits;
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("http server running");
+  });
