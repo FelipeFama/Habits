a4089d76e02c8b5ceb885fb537248c3af8fafd25
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-01-20 04:44:36 -0300

    Add new features in summarytable

--- frontend/src/components/SummaryTable.tsx
+++ frontend/src/components/SummaryTable.tsx
@@ -1,6 +1,12 @@
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
 import { HabitDay } from "./HabitDay";
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+
+const summaryDates = generateDatesFromYearBeginning();
+
+const minimunSummaryDatesSize = 18 * 7;
+const amountOfDaysToFill = minimunSummaryDatesSize - summaryDates.length;
 
 export function SummaryTable() {
   return (
@@ -18,20 +24,18 @@ export function SummaryTable() {
         })}
       </div>
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
+        {summaryDates.map((date) => {
+          return <HabitDay key={date.toString()} />;
+        })}
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+            return (
+              <div
+                key={i}
+                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+              ></div>
+            );
+          })}
       </div>
     </div>
   );
