8146688f2728af1abf32dc9b2fc6128ffde86500
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-01-20 14:54:13 -0300

    Habit completion route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,7 +1,7 @@
+import dayjs from "dayjs";
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { prisma } from "./lib/prisma";
-import dayjs from "dayjs";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/habits", async (request) => {
@@ -86,6 +86,45 @@ export async function appRoutes(app: FastifyInstance) {
     let day = await prisma.day.findUnique({
       where: {
         date: today,
+      },
+    });
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        },
+      });
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      },
+    });
+
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        },
+      });
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      });
+    }
+
+    await prisma.dayHabit.create({
+      data: {
+        day_id: day.id,
+        habit_id: id,
       },
     });
   });
