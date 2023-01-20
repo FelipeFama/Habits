ff4bb3ce74106d18f0bbc4b7601d95339dc3e534
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-01-19 23:18:38 -0300

    Add routes

--- backend/prisma/migrations/20230118064817_create_habits/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateTable
-CREATE TABLE "habits" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "tittle" TEXT NOT NULL,
-    "created_at" DATETIME NOT NULL
-);
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,19 +1,11 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
+import { appRoutes } from "./routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
-
-/* HTTP methods: GET, POST, PUT, Patch and Delete */
 
 app.register(cors);
-
-app.get("/hello", async () => {
-  const habits = await prisma.habit.findMany();
-
-  return habits;
-});
+app.register(appRoutes);
 
 app
   .listen({
