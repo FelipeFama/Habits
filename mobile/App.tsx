8fa4cd7f4e5b31fe0e622b3fdc63927730e7ed11
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-01-21 10:40:45 -0300

    Create routes mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -8,7 +8,7 @@ import {
   Inter_800ExtraBold,
 } from "@expo-google-fonts/inter";
 import { Loading } from "./src/components/Loading";
-import { Home } from "./src/screens/Home";
+import { Routes } from "./src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -24,7 +24,7 @@ export default function App() {
 
   return (
     <>
-      <Home />
+      <Routes />
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
