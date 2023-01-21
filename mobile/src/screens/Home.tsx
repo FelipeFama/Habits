37e30ae15f070ec67a85522bd2ffc4fb035fe5e6
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-01-21 09:24:15 -0300

    Generate days component

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,9 +1,25 @@
-import { View } from "react-native";
+import { Text, View, ScrollView } from "react-native";
 import { Header } from "../components/Header";
+import { HabitDay, DAY_SIZE } from "../components/HabitDay";
+
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+
 export function Home() {
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
+      <View className="flex-row mt-6 mb-2">
+        {weekDays.map((weekDay, i) => (
+          <Text
+            key={`${weekDay}-${i}`}
+            className="text-zinc-400 text-xl font-bold text-center mx-1"
+            style={{ width: DAY_SIZE }}
+          >
+            {weekDay}
+          </Text>
+        ))}
+      </View>
+      <HabitDay />
     </View>
   );
 }
