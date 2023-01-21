1cc960a0906b7fe686bb0a8a991654e304d2c945
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-01-21 05:04:29 -0300

    Adding features in form

--- frontend/src/components/NewHabitForm.tsx
+++ frontend/src/components/NewHabitForm.tsx
@@ -1,5 +1,6 @@
 import * as Checkbox from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
+import { FormEvent, useState } from "react";
 
 const availableWeekDays = [
   "Domingo",
@@ -12,9 +13,24 @@ const availableWeekDays = [
 ];
 
 export function NewHabitForm() {
-  function createNewHabit() {
-    console.log("create");
+  const [title, setTitle] = useState("");
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  function createNewHabit(event: FormEvent) {
+    console.log(title, weekDays);
+    event.preventDefault();
   }
+
+  function handleToggleWeekDay(weekDay: number) {
+    if (weekDays.includes(weekDay)) {
+      const weekDaysRemovedOne = weekDays.filter((day) => day !== weekDay);
+      setWeekDays(weekDaysRemovedOne);
+    } else {
+      const weekDaysWithAddedOne = [...weekDays, weekDay];
+      setWeekDays(weekDaysWithAddedOne);
+    }
+  }
+
   return (
     <form onSubmit={createNewHabit} className="w-full flex flex-col mt-6">
       <label htmlFor="title" className="ont-semibold leading-tight">
@@ -26,17 +42,19 @@ export function NewHabitForm() {
         placeholder="ex.: Exercícios, dormir bem, etc..."
         className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
         autoFocus
+        onChange={(event) => setTitle(event.target.value)}
       />
       <label htmlFor="" className="font-semibold leading-tight mt-4">
         Qual a recorrência ?
       </label>
 
       <div className="flex flex-col gap-2 mt-3">
-        {availableWeekDays.map((weekDay) => {
+        {availableWeekDays.map((weekDay, index) => {
           return (
             <Checkbox.Root
               key={weekDay}
               className="flex items-center gap-3 group"
+              onCheckedChange={() => handleToggleWeekDay(index)}
             >
               <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-50">
                 <Checkbox.Indicator>
