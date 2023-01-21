16135f932ba24f65bf7072a61b2f870ef43397c0
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-01-21 11:32:07 -0300

    New component completed

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,5 +1,24 @@
-import { View } from "react-native";
+import { ScrollView, Text, TextInput, View } from "react-native";
+import { BackButton } from "../components/BackButton";
+import colors from "tailwindcss/colors";
 
 export function New() {
-  return <View className="flex-1 bg-background px-8 pt-16"></View>;
+  return (
+    <View className="flex-1 bg-background px-8 pt-16">
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <BackButton />
+        <Text className="mt-6 text-white font-extrabold text-3xl">
+          Criar Hábito
+        </Text>
+        <Text className="mt-6 text-white font-semibold text-base">
+          Qual seu comprometimento?
+        </Text>
+        <TextInput
+          className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600"
+          placeholder="Exercícios, dormir bem, etc..."
+          placeholderTextColor={colors.zinc[400]}
+        />
+      </ScrollView>
+    </View>
+  );
 }
