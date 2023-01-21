6c9307ffb3b945dd313d773c6e8dde19e93c26e6
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-01-21 09:01:42 -0300

    Style header mobile

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,4 +1,9 @@
 import { View } from "react-native";
+import { Header } from "../components/Header";
 export function Home() {
-  return <View className="flex-1 bg-background px-8 pt-16"></View>;
+  return (
+    <View className="flex-1 bg-background px-8 pt-16">
+      <Header />
+    </View>
+  );
 }
