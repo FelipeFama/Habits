25195fa0da5f97cd893f9c534271b32379de46e0
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-01-22 08:38:57 -0300

    Update Habit.tsx

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useRoute } from "@react-navigation/native";
 import { Alert, ScrollView, Text, View } from "react-native";
 import dayjs from "dayjs";
+import clsx from "clsx";
 import { BackButton } from "../components/BackButton";
 import { ProgressBar } from "../components/ProgressBar";
 import { CheckBox } from "../components/CheckBox";
@@ -31,6 +32,7 @@ export function Habit() {
   const { date } = route.params as Params;
 
   const parsedDate = dayjs(date);
+  const isDateInPast = parsedDate.endOf("day").isBefore(new Date());
   const dayOfWeek = parsedDate.format("dddd");
   const dayAndMonth = parsedDate.format("DD/MM");
 
@@ -94,13 +96,18 @@ export function Habit() {
 
         <ProgressBar progress={habitsProgress} />
 
-        <View className="mt-6">
+        <View
+          className={clsx("mt-6", {
+            ["opacity-50"]: isDateInPast,
+          })}
+        >
           {dayInfo?.possibleHabits ? (
             dayInfo?.possibleHabits.map((habit) => (
               <CheckBox
                 key={habit.id}
                 title={habit.title}
                 checked={completedHabits?.includes(habit.id)}
+                disabled={isDateInPast}
                 onPress={() => handleToggleHabits(habit.id)}
               />
             ))
@@ -108,6 +115,11 @@ export function Habit() {
             <HabitsEmpty />
           )}
         </View>
+        {isDateInPast && (
+          <Text className="text-white mt-10 text-center">
+            Você não pode editar hábitos de uma data passada.
+          </Text>
+        )}
       </ScrollView>
     </View>
   );
